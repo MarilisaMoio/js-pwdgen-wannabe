655f0f8d9473f33ea5f5f32679f2c2db62185c29
MarilisaMoio <moiomarilisa@gmail.com>
2024-03-04 14:37:48 +0100

    feat: add generator

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 // Obiettivo: ottenere una psw composta dal nome dell'utente, il cognome, il suo colore preferito e il numero 23
+
+//raccolta dati
+
+const userName = prompt("Inserisci qui il tuo nome senza maiuscole");
+
+const userSrname = prompt("Inserisci qui il tuo cognome senza maiuscole");
+
+const userColor = prompt("Inserisci qui il tuo colore preferito senza maiuscole");
+
+//output
+document.getElementById("pswContainer").innerHTML = userName + userSrname + userColor + "23";
