327db10039646c4afa8918d97d44004a39c35d33
MarilisaMoio <moiomarilisa@gmail.com>
2024-03-04 16:43:29 +0100

    feat: add bonus

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
 // Obiettivo: ottenere una psw composta dal nome dell'utente, il cognome, il suo colore preferito e il numero 23
 
 //raccolta dati
-
 const userName = prompt("Inserisci qui il tuo nome senza maiuscole");
 
 const userSrname = prompt("Inserisci qui il tuo cognome senza maiuscole");
 
 const userColor = prompt("Inserisci qui il tuo colore preferito senza maiuscole");
 
+//bonus
+const userNumberStr = prompt("Inserisci qui un numero");
+
+const userNumber = parseInt(userNumberStr);
+
+const finalNumber = userNumber + 23;
+
 //output
-document.getElementById("pswContainer").innerHTML = userName + userSrname + userColor + "23";
+document.getElementById("pswContainer").innerHTML = userName + userSrname + userColor + finalNumber;
 
 document.getElementById("yay").innerHTML = "&star; yay! &star;";
